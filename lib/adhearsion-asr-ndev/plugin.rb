641f7d34a60b195d898e45bec5e47c500544b80f
Ben Klang <bklang@mojolingo.com>
2014-01-13 20:56:27 -0500

    Ndev has no concept of a grammar :(

--- lib/adhearsion-asr-ndev/plugin.rb
+++ lib/adhearsion-asr-ndev/plugin.rb
@@ -3,7 +3,7 @@ module AdhearsionASR::Ndev
   class Plugin < Adhearsion::Plugin
     config :adhearsion_asr_ndev do
       auto_include true, transform: Proc.new { |v| v == 'true' }, desc: "Enable or disable auto inclusion of overridden Adhearsion Core methods in all call controllers."
-      min_confidence 0.5, desc: 'The default minimum confidence level used for all recognizer invocations.', transform: Proc.new { |v| v.to_f }
+      #min_confidence 0.5, desc: 'The default minimum confidence level used for all recognizer invocations.', transform: Proc.new { |v| v.to_f }
       timeout 5, desc: 'The default timeout (in seconds) used for all recognizer invocations.', transform: Proc.new { |v| v.to_i }
       input_language 'en-US', desc: 'The default language set on generated grammars. Set nil to use platform default.'
       app_id '', desc: 'Nuance NDEV App ID'
